1ed46b770007ae5d3d24c6fd2c486be272b2e8d6
samaraufersa <109767273+samaraufersa@users.noreply.github.com>
2025-09-09 21:56:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(
-    page_title="DashCovid",
+    page_title="DASHCOVID - Um dasboard para análise de dados",
     layout="wide")
 
 df = pd.read_csv('WHO_time_series_projeto.csv')
